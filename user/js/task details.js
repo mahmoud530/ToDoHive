9c1f11293877c44808e9c711d7f62319c843f42b
Allam <92165435+mahmoud530@users.noreply.github.com>
2024-08-22 07:28:22 +0300

    Add files via upload

---
+++ user/js/task details.js
@@ -0,0 +1,24 @@
+var mainPop1 = document.getElementById("mainPop1")
+var pop2 = document.getElementById("mainPop2")
+
+function closePop1() {
+    mainPop1.classList.add("d-none")
+}
+
+function openPop1() {
+    mainPop1.classList.remove("d-none")
+}
+
+
+
+function closePop2() {
+    pop2.classList.add("d-none")
+}
+
+
+function openPop2() {
+    pop2.classList.remove("d-none")
+}
+
+
+console.log("pop2");
